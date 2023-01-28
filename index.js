63b990ce6e1c708896d34147fe75fa5ec03a0d99
jacksonohallo <jacksonohallo@gmail.com>
2023-01-28 11:23:51 +0300

    add cors to allow credential login

--- index.js
+++ index.js
@@ -15,16 +15,9 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wtegw345jwerjktjwertkj';
 
-app.use(cors());
+app.use(cors({origin: 'https://client-delta-tan.vercel.app', credentials: true}))
 
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://client-delta-tan.vercel.app')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
 
 app.use(express.json());
 app.use(cookieParser());
